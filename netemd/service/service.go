5b094d6befeebe1114523367337ccb3dd4c84fa0
buehlert <buehlert@student.ethz.ch>
2019-01-28 18:43:29 +0100

    fixed loop problem

--- netemd/service/service.go
+++ netemd/service/service.go
@@ -141,9 +141,12 @@ func hpingPoller(cfg *config.Config) {
 func plusPoller(cfg *config.Config) {
 	currentLine := 0
 	countSame := 0
-	var oldValid, oldInvalid, temp int64
+	oldValid := []int64{0, 0}
+	oldInvalid := []int64{0, 0}
+	temp := []int64{0, 0}
+
 	for {
-		for _, iface := range cfg.Interfaces {
+		for i, iface := range cfg.Interfaces {
 
 			// err := plus.Fetch("/root/share/vagrant_test/vagrant/spinbit_plus_printf.out", currentLine)
 			// err := plus.Fetch(currentLine)
@@ -171,20 +174,20 @@ func plusPoller(cfg *config.Config) {
 			// 	_, _ = fOut.WriteString("\n")
 			// }
 
-			if plusData.Valid >= oldValid {
-				temp = plusData.Valid
-				plusData.Valid = plusData.Valid - oldValid
-				oldValid = temp
+			if plusData.Valid >= oldValid[i] {
+				temp[i] = plusData.Valid
+				plusData.Valid = plusData.Valid - oldValid[i]
+				oldValid[i] = temp[i]
 			} else {
-				oldValid = plusData.Valid
+				oldValid[i] = plusData.Valid
 			}
 
-			if plusData.Invalid > oldInvalid {
-				temp = plusData.Invalid
-				plusData.Invalid = plusData.Invalid - oldInvalid
-				oldInvalid = temp
+			if plusData.Invalid > oldInvalid[i] {
+				temp[i] = plusData.Invalid
+				plusData.Invalid = plusData.Invalid - oldInvalid[i]
+				oldInvalid[i] = temp[i]
 			} else {
-				oldInvalid = plusData.Invalid
+				oldInvalid[i] = plusData.Invalid
 			}
 
 			if currentLine == newLine {
